3f1f6711743acbd061ff8be80ec85a50898ef166
Ali Rahimi <arahimihajiabadi@gmail.com>
2021-01-06 12:59:15 +0330

    Copy-Pasted some common code!

---
+++ resources/views/base.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <title>@yield('title', env('APP_NAME', 'Blog'))</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}"/>
+    <link rel="stylesheet" href="{{asset('css/fonts.css')}}"/>
+    <link rel="stylesheet" href="{{asset('css/icomoon.css')}}"/>
+    <meta name="description" content="@yield('description', '')"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    @yield('head')
+</head>
+<body>
+    @yield('body')
+    <script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
