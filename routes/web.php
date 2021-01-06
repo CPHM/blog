db06d7d4a8ea2e0e6f93edf3b84efaf7fde21909
Ali Rahimi <arahimihajiabadi@gmail.com>
2021-01-06 19:21:56 +0330

    Added most(or maybe all) web routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,4 +16,21 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::view('/', 'posts.list')->name('home');
+
+Route::view('/login', 'auth.login')->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::view('/password/request', 'auth.forgot-password')->name('password.request');
+Route::post('/password/request', [AuthController::class, 'getResetEmail']);
+Route::get('/password/reset/{token}', [AuthController::class, 'resetForm']);
+Route::post('/password/reset', [AuthController::class, 'resetPassword'])->name('password.request');
+
+Route::resource('posts', PostController::class);
+
+Route::resource('categories', CategoryController::class);
+
+Route::resource('users', UserController::class)->middleware('auth');
+
 
