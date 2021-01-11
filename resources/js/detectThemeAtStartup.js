de759bb89c072dd6f370c4879b393894b69aaa8c
Ali Rahimi <arahimihajiabadi@gmail.com>
2021-01-11 07:09:03 +0330

    Made startup theme detection immediate

--- resources/js/detectThemeAtStartup.js
+++ resources/js/detectThemeAtStartup.js
@@ -4,15 +4,13 @@
  * Side Effect: Sets html class dark
  */
 module.exports = function () {
-    window.addEventListener('load', function () {
-       let dark = false;
-       if (window.matchMedia('(prefers-color-scheme: dark)').matches)
-           dark = true;
-       if (localStorage.getItem('theme'))
-           dark = localStorage.getItem('theme') === 'dark';
-       if (dark)
-           document.querySelector('html').classList.add('dark');
-       else
-           document.querySelector('html').classList.remove('dark');
-    });
+    let dark = false;
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches)
+        dark = true;
+    if (localStorage.getItem('theme'))
+        dark = localStorage.getItem('theme') === 'dark';
+    if (dark)
+        document.querySelector('html').classList.add('dark');
+    else
+        document.querySelector('html').classList.remove('dark');
 }
